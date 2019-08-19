cdc52188f683919b3f3024d174fbd659c53bfc0f
姚尧 <yaoyao2@douyu.tv>
2019-08-19 18:37:19 +0800

    加入redux测试。

--- src/pages/Index/index.jsx
+++ src/pages/Index/index.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
-import Description from 'components/Description';
+import React, { useState } from 'react';
+// import Description from 'components/Description';
 import './index.less';
 
 export default class Index extends React.PureComponent {
+    // constructor(props) {
+    //     super(props);
+    //     const [title] = useState(0);
+    //     this.state = {
+    //         title: '',
+    //     };
+    // }
+
+    mapStateToProps(state) {
+        return {
+            title: state.title,
+        };
+    }
+
+    mapDispatchToProps(dispatch) {
+        return {
+            changeTitle: () => { this._changeTitle(dispatch); },
+        };
+    }
+
+    _changeTitle(dispatch) {
+        const [title] = useState(0);
+        console.log(dispatch, title);
+        // dispatch();
+    }
+
     render() {
-        return <Description />;
+        // return <Description />;
+        const { title = '' } = this.state || {};
+        return (
+            <div>
+                {title}
+            </div>
+        );
     }
 }
