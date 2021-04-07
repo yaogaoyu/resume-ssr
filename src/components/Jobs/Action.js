96ea9e24c76c1bec6dcdb0e918b8aef8b27ee1af
Yao <yaogaoyu@gmail.com>
2021-04-07 16:31:21 +0800

    修正排序后的引用

--- src/components/Jobs/Action.js
+++ src/components/Jobs/Action.js
@@ -11,9 +11,9 @@ import { findArray } from 'core/mongo/MongoClient';
  */
 export const getJobs = async () => {
     // const result = await ApiClient.get('api test1');
-    const result = await findArray('jobs', {});
+    let result = await findArray('jobs', {});
     // eslint-disable-next-line no-underscore-dangle
-    result.sort((a, b) => { return b._id - a._id; });
+    result = result.sort((a, b) => { return b._id - a._id; });
     return {
         type: 'jobs-init-data',
         data: result,
